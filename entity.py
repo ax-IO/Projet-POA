d9469e3597d75db0347f1711431bafa93bf58262
pierrhum <pierrhum@debian-BULLSEYE-live-builder-AMD64>
2021-10-11 12:54:19 +0200

    move player move function in entity class

--- entity.py
+++ entity.py
@@ -2,6 +2,13 @@ class Entity():
     def __init__(self, grid, pos = (2, 7)):
         self.grid = grid
         self.pos = pos
-    def move(self, off_x, off_y):
-        self.pos[0] += off_x
-        self.pos[1] += off_y
+        self.moved = False
+    def move(self, x, y):
+        posX = self.pos[0] + x
+        posY = self.pos[1] + y
+
+        if (posX>=0 and posX<len(self.grid[0]) and posY>=0 and posY<len(self.grid[0])):
+            if (self.grid[posX][posY] != 'W') :
+                self.grid[self.pos[0]][self.pos[1]] = 0
+                self.pos = (posX, posY)
+                self.moved = True
--- player.py
+++ player.py
@@ -3,21 +3,9 @@ from entity import Entity
 class Player(Entity):
     def __init__(self, grid, pos = (8, 5)):
         super(Player, self).__init__(grid, pos)
-        self.moved = False
+        
     def move (self, x, y):
-        if(self.grid[self.pos[0]][self.pos[1]] == 'T'):
-            self.grid[self.pos[0]][self.pos[1]] = 'V'
-        else:
-            self.grid[self.pos[0]][self.pos[1]] = 0
+        super(Player, self).move(x,y)
 
-        posX = self.pos[0] + x
-        posY = self.pos[1] + y
-
-        if (posX>=0 and posX<len(self.grid[0]) and posY>=0 and posY<len(self.grid[0])):
-            if (self.grid[posX][posY] != 'W') :
-                self.pos = (posX, posY)
-                self.moved = True
-        if(self.grid[self.pos[0]][self.pos[1]] == 'V'):
-            self.grid[self.pos[0]][self.pos[1]] = 'T'
-        elif(self.grid[self.pos[0]][self.pos[1]] != 'H'):
+        if(self.moved and self.grid[self.pos[0]][self.pos[1]] != 'H'):
             self.grid[self.pos[0]][self.pos[1]] = 'P'
