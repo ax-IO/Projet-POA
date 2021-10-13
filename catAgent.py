cecc52f6b63c115c9508707d5e4c562c0f65f8e2
pierrhum <pierrhum@debian-BULLSEYE-live-builder-AMD64>
2021-10-13 18:28:35 +0200

    update_cone_vision_refactor

--- catAgent.py
+++ catAgent.py
@@ -2,6 +2,8 @@
 import pygame
 import random
 
+from pygame.constants import VIDEOEXPOSE
+
 from entity import Entity
 
 
@@ -34,10 +36,14 @@ class Cat(Entity):
         if (turn_count % 2 == 1):
             self.move()
 
+    def chase(self):
+        print("not implemented yet")
 
     def choix_action(self, turn_count):
         if(self.state==0): # Patrolling
             self.patrol(turn_count)
+        elif(self.state==1): # Chasing player
+            self.chase()
 
     def move(self):
         if (self.direction == 0): # Right
@@ -60,6 +66,32 @@ class Cat(Entity):
             self.direction = self.direction % 360
         self.update_cone_vision()
 
+    def build_cone_vision(self, minDist, maxDist):
+        Vx = Vy = 0
+        
+        for d in range(minDist, maxDist):
+            # Determine width according to vision distance
+            if (self.direction == 0 or self.direction == 270):
+                minWidth = -d + 1
+                maxWidth = d
+            elif (self.direction == 90 or self.direction == 180):
+                minWidth = d + 1
+                maxWidth = -d
+
+            for w in range(minWidth, maxWidth):
+                # Manage sides vision cones
+                if ((self.direction == 0 or self.direction == 180) 
+                and self.pos[0]+w >= 0 and self.pos[0]+w < len(self.grid[0]) and self.pos[1]+d >= 0 and self.pos[1] + d < len(self.grid[0])):
+                    Vx = self.pos[0] + w
+                    Vy = self.pos[1] + d
+                    self.vision[Vx][Vy] = 'V'
+
+                elif ((self.direction == 90 or self.direction == 270) 
+                and self.pos[0]+d >= 0 and self.pos[0]+d < len(self.grid[0]) and self.pos[1]+w >= 0 and self.pos[1] + w < len(self.grid[0])):
+                    Vx = self.pos[0] + d
+                    Vy = self.pos[1] + w
+                    self.vision[Vx][Vy] = 'V'
+        
     def update_cone_vision(self):
         # Nettoie cases entourant le chat
         for j in range(-self.portee_vision, self.portee_vision+1):
@@ -68,37 +100,16 @@ class Cat(Entity):
                     if (self.vision[self.pos[0]+j][self.pos[1]+i] == 'V'):
                         self.vision[self.pos[0]+j][self.pos[1]+i] = 0
 
-        # Crée les cases de visions
-        # if (self.direction  == 90):
-        #     grid[self.pos[0]][self.pos[1] +1] = 'V'
-
         if (self.direction == 0):
-            for dist in range(1, self.portee_vision+1):
-                for largeur in range(-dist + 1, dist):
-                    if (self.pos[0]+largeur >= 0 and self.pos[0]+largeur < len(self.grid[0]) and self.pos[1]+dist >= 0 and self.pos[1] + dist < len(self.grid[0])):
-                        self.vision[self.pos[0] +
-                                    largeur][self.pos[1] + dist] = 'V'
+            self.build_cone_vision(1, self.portee_vision+1)
 
-        if (self.direction == 90):
-            for dist in range(-self.portee_vision, 0):
-                for largeur in range(dist + 1, -dist):
-                    # print("dist=", dist, "largeur=", largeur)
-                    if (self.pos[0]+dist >= 0 and self.pos[0]+dist < len(self.grid[0]) and self.pos[1]+largeur >= 0 and self.pos[1] + largeur < len(self.grid[0])):
-                        self.vision[self.pos[0] +
-                                    dist][self.pos[1] + largeur] = 'V'
+        elif (self.direction == 90):
+            self.build_cone_vision(-self.portee_vision, 0)
 
-        if (self.direction == 180):
-            for dist in range(-self.portee_vision, 0):
-                for largeur in range(dist + 1, -dist):
-                    # print("dist=", dist, "largeur=", largeur)
-                    if (self.pos[0]+largeur >= 0 and self.pos[0]+largeur < len(self.grid[0]) and self.pos[1]+dist >= 0 and self.pos[1] + dist < len(self.grid[0])):
-                        self.vision[self.pos[0] +
-                                    largeur][self.pos[1] + dist] = 'V'
-
-        if (self.direction == 270):
-            for dist in range(1, self.portee_vision+1):
-                for largeur in range(-dist + 1, dist):
-                    if (self.pos[0]+dist >= 0 and self.pos[0]+dist < len(self.grid[0]) and self.pos[1]+largeur >= 0 and self.pos[1] + largeur < len(self.grid[0])):
-                        self.vision[self.pos[0] +
-                                    dist][self.pos[1] + largeur] = 'V'
+        elif (self.direction == 180):
+            self.build_cone_vision(-self.portee_vision, 0)
+            
+        elif (self.direction == 270):
+            self.build_cone_vision(1, self.portee_vision+1)
+                        
     # [-dist+1 ; dist]
